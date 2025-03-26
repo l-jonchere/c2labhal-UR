0ab5943b67a4b333a23a4484912d51b11de0ad56
guillaumegodet <guillaumegodet@gmail.com>
2025-03-26 11:50:49 +0100

    For publications found in HAL, now retrieves the submit type to check if fulltext is available or if there is only a metadata notice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ from metapub import PubMedFetcher
 import regex as re
 from unidecode import unidecode
 from langdetect import detect
+from tqdm import tqdm
+
+# Configurer tqdm pour pandas
+tqdm.pandas()
 
 # Fonction pour récupérer les données de Scopus
 def get_scopus_data(api_key, query, max_items=2000):
@@ -101,95 +105,198 @@ def clean_doi(doi):
     return doi
 
 # Fonction pour récupérer les données HAL
-def safe_get_json(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        try:
-            return response.json()
-        except requests.exceptions.JSONDecodeError:
-            print(f"Erreur de décodage JSON pour l'URL : {url}")
-            return None
-    else:
-        print(f"Erreur API : {response.status_code} pour l'URL : {url}")
-        return {}
+endpoint="http://api.archives-ouvertes.fr/search/"
 
-def get_hal_data(collection_a_chercher, start_year, end_year):
-    endpoint = "http://api.archives-ouvertes.fr/search/"
-    n = safe_get_json(f"{endpoint}{collection_a_chercher}/?q=*&fq=publicationDateY_i:[{start_year} TO {end_year}]&fl=doiId_s,title_s&rows=0&sort=docid asc&wt=json")
-    n = n.get('response', {}).get('numFound', 0)
-    print(f'Publications trouvées : {n}')
+escapeRules ={'+':r'\+','-':r'\-','&':r'\&','|':r'\|','!':r'\!','(':r'\(',')':r'\)','{':r'\{','}':r'\}','[':r'\[',
+              ']':r'\]','^':r'\^','~':r'\~','*':r'\*','?':r'\?',':':r'\:','"':r'\"'}
 
-    dois_coll = []
-    titres_coll = []
+hal_fl="docid,doiId_s,title_s,submitType_s"
 
-    if n > 1000:
-        current = 0
-        cursor = ""
-        next_cursor = "*"
-        while cursor != next_cursor:
-            print(f"\rEn cours : {current}", end="\t")
-            cursor = next_cursor
-            page = safe_get_json(f"{endpoint}{collection_a_chercher}/?q=*&fq=publicationDateY_i:[{start_year} TO {end_year}]&fl=doiId_s,title_s&rows=1000&cursorMark={cursor}&sort=docid asc&wt=json")
-            for d in page.get('response', {}).get('docs', []):
-                for t in d.get('title_s', []):
-                    titres_coll.append(t)
-                dois_coll.append(d.get('doiId_s', '').lower())
-            current += 1000
-            next_cursor = page.get('nextCursorMark', '')
-    else:
-        page = safe_get_json(f"{endpoint}{collection_a_chercher}/?q=*&fq=publicationDateY_i:[{start_year} TO {end_year}]&fl=doiId_s,title_s&rows=1000&sort=docid asc&wt=json")
-        for d in page.get('response', {}).get('docs', []):
-            for t in d.get('title_s', []):
-                titres_coll.append(t)
-            dois_coll.append(d.get('doiId_s', '').lower())
+default_start=2018
+default_end='*'
+def escapedSeq(term):
+    """ Yield the next string based on the
+    next character (either this char
+    or escaped version """
+    for char in term:
+        if char in escapeRules.keys():
+            yield escapeRules[char]
+        else:
+            yield char
 
-    print(f"\rTerminé : {n} publications chargées", end="\t")
-    return dois_coll, titres_coll
+def escapeSolrArg(term):
+    """ Apply escaping to the passed in query terms
+    escaping special characters like : , etc."""
+    term = term.replace('\\',r'\\') # escape \ first
+    return"".join([nextStr for nextStr in escapedSeq(term)])
 
 def normalise(s):
-    return re.sub(' +', ' ', unidecode(re.sub(r'\W', ' ', s))).lower()
+    """Takes any string and returns it with only normal characters, single spaces and in lower case."""
+    return re.sub(' +', ' ',unidecode(re.sub(r'\W',' ', s))).lower()
 
-def inex_match(nti, nti_coll):
-    for x in nti_coll:
-        if len(nti) * 1.1 > len(x) and len(nti) * 0.9 < len(x):
-            if re.fullmatch(f"({nti}){{e<={int(len(x) / 20)}}}", x):
-                return True
+def compare_inex(nti,cti):
+    """Takes a normalised title from the list to be compared, and compares it with the title from the list extracted from HAL.
+    Returns True if the titles have comparable lengths and 90% character similarity (~Lehvenstein distance)"""
+    nti=normalise(nti).strip()
+    if len(nti)*1.1 > len(cti) > len(nti)*0.9:
+        if len(cti) > 50:
+            if re.fullmatch("("+nti[:50]+"){e<=5}",cti[:50]):
+                return cti if  re.fullmatch("("+nti+"){"+f"e<={int(len(cti)/10)}"+"}",cti) else False
+        else:
+            return cti if  re.fullmatch("("+nti+"){"+f"e<={int(len(cti)/10)}"+"}",cti) else False
     return False
 
-def statut_titre(ti, nti_coll):
-    if isinstance(ti, str) and ti.endswith("]"):
-        try:
-            if detect(ti[:re.match(".*\\[", ti).span()[1]]) != detect(ti[re.match(".*\\[", ti).span()[1]:]):
-                ti = ti[re.match(".*\\[", ti).span()[1]:]
-        except:
-            pass
+def ex_in_coll(ti,coll_df):
+    """Takes a title from the list to be compared. If it is in the list of titles from the compared HAL collection,
+    returns the corresponding HAL reference. Else, returns False."""
     try:
-        nti = normalise(ti)
-    except TypeError:
-        return "titre invalide"
-    if nti in nti_coll:
-        return "titre trouvé dans la collection : probablement déjà présent"
-    elif inex_match(nti, nti_coll):
-        return "titre approchant trouvé dans la collection : à vérifier"
-    else:
-        hal_result = safe_get_json(f"http://api.archives-ouvertes.fr/search/?q=title_t:{nti}&rows=0")
-        if hal_result.get('response', {}).get('numFound', 0) > 0:
-            return "titre trouvé dans HAL mais hors de la collection : affiliation probablement à corriger"
-        else:
-            return "hors HAL"
+        return ["Titre trouvé dans la collection : probablement déjà présent",
+                ti,
+                coll_df[coll_df['Titres']==ti].iloc[0,0],
+                coll_df[coll_df['Titres']==ti].iloc[0,3]]
+    except IndexError:
+        return False
 
-def statut_doi(do, dois_coll):
-    if pd.isna(do):
-        return "pas de DOI valide"
-    ndo = do.replace("https://doi.org/", "").lower()
-    if ndo in dois_coll:
-        return "Dans la collection"
-    else:
-        hal_result = safe_get_json(f"http://api.archives-ouvertes.fr/search/?q=doiId_id:{ndo}&rows=0")
-        if hal_result.get('response', {}).get('numFound', 0) > 0:
-            return "Dans HAL mais hors de la collection"
+def inex_in_coll(nti,coll_df):
+    """Takes a title from the list to be compared. If it has at least 90% similarity with one of the titles from the compared HAL collection,
+    returns the corresponding HAL reference. Else, returns False."""
+    for x in list(coll_df['nti']):
+        y = compare_inex(nti,x)
+        if y:
+            return ["Titre approchant trouvé dans la collection : à vérifier",
+                    coll_df[coll_df['nti']==y].iloc[0,2],
+                    coll_df[coll_df['nti']==y].iloc[0,0],
+                    coll_df[coll_df['nti']==y].iloc[0,3]]
+    return False
+
+def in_hal(nti,ti):
+    """Tries to find a title in HAL, first with a strict character match then if not found with a loose SolR search"""
+    try:
+        r_ex=requests.get(f"{endpoint}?q=title_t:{nti}&rows=1&fl={hal_fl}").json()['response']
+        if r_ex['numFound'] >0:
+            if any(ti==x for x in r_ex['docs'][0]['title_s']):
+                return ["Titre trouvé dans HAL mais hors de la collection : affiliation probablement à corriger",
+                        r_ex['docs'][0]['title_s'][0],
+                        r_ex['docs'][0]['docid'],
+                        r_ex['docs'][0]['submitType_s']]
+    except KeyError:
+        r_inex=requests.get(f"{endpoint}?q=title_t:{ti}&rows=1&fl={hal_fl}").json()['response']
+        if r_inex['numFound'] >0:
+            return ["Titre approchant trouvé dans HAL mais hors de la collection : vérifier les affiliations",
+                    r_inex['response']['docs'][0]['title_s'][0],
+                    r_inex['response']['docs'][0]['docid'],
+                    r_ex['docs'][0]['submitType_s']] if any(
+                        compare_inex(ti,x) for x in [r_inex['response']['docs'][0]['title_s']]
+                        ) else ["Hors HAL","","",""]
+    return ["Hors HAL","","",""]
+
+def statut_titre(title,coll_df):
+    """Applies the matching process to a title, from searching it exactly in the HAL collection to be compared, to searching it loosely in HAL search API."""
+    try:
+        if title[len(title)-1]=="]" and detect(title[:re.match(r".*\[",ti).span()[1]]) != detect(title[re.match(r".*\[",title).span()[1]:]):
+            title=title[re.match(r".*\[",title).span()[1]:]
+        elif detect(title[:len(title)/2]) != detect(title[len(title)/2:]):
+            title=title[:len(title)/2]
+        else: title=title
+    except:
+        title=title
+    try:
+        ti='\"'+escapeSolrArg(title)+'\"'
+    except TypeError:
+        return ["Titre invalide","","",""]
+    try:
+        c_ex=ex_in_coll(title,coll_df)
+        if c_ex:
+            return c_ex
         else:
-            return "hors HAL"
+            c_inex = inex_in_coll(title,coll_df)
+            if c_inex:
+                return c_inex
+            else:
+                r_ex=in_hal(ti,title)
+                return r_ex
+    except KeyError:
+        return ["Titre incorrect, probablement absent de HAL","","",""]
+
+def statut_doi(do,coll_df):
+    """applies the matching process to a DOI, searching it in the collection to be compared then in all of HAL"""
+    dois_coll=coll_df['DOIs'].tolist()
+    if do and do == do:
+        ldo=do.lower()
+        ndo=escapeSolrArg(re.sub(r"\[.*\]","",do.replace("https://doi.org/","").lower()))
+        if ldo in dois_coll:
+            return ["Dans la collection",
+                    coll_df[coll_df['DOIs']==ldo].iloc[0,2],
+                    coll_df[coll_df['DOIs']==ldo].iloc[0,0],
+                    coll_df[coll_df['DOIs']==ldo].iloc[0,3]]
+        else:
+            r=requests.get(f"{endpoint}?q=doiId_id:{ndo}&rows=1&fl={hal_fl}").json()
+            if r['response']['numFound'] >0:
+                return ["Dans HAL mais hors de la collection",
+                        r['response']['docs'][0]['title_s'][0],
+                        r['response']['docs'][0]['docid'],
+                        r['response']['docs'][0]['submitType_s']]
+            return ["Hors HAL","","",""]
+    elif do != do:
+        return ["Pas de DOI valide","","",""]
+
+def check_df(df,coll_df):
+    """Applies the full process to the dataframe or table given as an input."""
+    results = df.progress_apply(
+        lambda x: statut_doi(x['doi'], coll_df) if statut_doi(x['doi'], coll_df)[0] in ("Dans la collection", "Dans HAL mais hors de la collection") else statut_titre(x['Title'], coll_df), axis=1
+    )
+    df['Statut'] = results.apply(lambda x: x[0])
+    df['titre_si_trouvé'] = results.apply(lambda x: x[1])
+    df['identifiant_hal_si_trouvé'] = results.apply(lambda x: x[2])
+    df['statut_dépôt_si_trouvé'] = results.apply(lambda x: x[3])
+
+class HalCollImporter:
+
+    def __init__(self,coll_code:str,start_year=None,end_year=None):
+        self.coll_code=coll_code
+        self.start_year=start_year if start_year != None else default_start
+        self.end_year=end_year if end_year != None else default_end
+        self.nbdocs=self.get_nb_docs()
+
+    def get_nb_docs(self):
+        n=requests.get(f"{endpoint}{self.coll_code}/?q=*&fq=publicationDateY_i:[{self.start_year} TO {self.end_year}]&fl=docid&rows=0&sort=docid asc&wt=json").json()['response']['numFound']
+        return n
+
+    def import_data(self):
+        n=self.nbdocs
+        docid_coll=list()
+        dois_coll=list()
+        titres_coll=list()
+        submit_types_coll=list()
+        if n>1000:
+            current=0
+            cursor=""
+            next_cursor="*"
+            while cursor != next_cursor:
+                cursor=next_cursor
+                page=requests.get(f"{endpoint}{self.coll_code}/?q=*&fq=publicationDateY_i:[{self.start_year} TO {self.end_year}]&fl={hal_fl}&rows=1000&cursorMark={cursor}&sort=docid asc&wt=json").json()
+                for d in page['response']['docs']:
+                    for t in d['title_s']:
+                        titres_coll.append(t)
+                        docid_coll.append(d['docid'])
+                        try:
+                            dois_coll.append(d['doiId_s'].lower())
+                        except KeyError:
+                            dois_coll.append("")
+                        submit_types_coll.append(d['submitType_s'])
+                current+=1000
+                next_cursor=page['nextCursorMark']
+        else:
+            for d in requests.get(f"{endpoint}{self.coll_code}/?q=*&fq=publicationDateY_i:[{self.start_year} TO {self.end_year}]&fl={hal_fl}&rows=1000&sort=docid asc&wt=json").json()['response']['docs']:
+                for t in d['title_s']:
+                    titres_coll.append(t)
+                    docid_coll.append(d['docid'])
+                    try:
+                        dois_coll.append(d['doiId_s'].lower())
+                    except KeyError:
+                        dois_coll.append("")
+                    submit_types_coll.append(d['submitType_s'])
+        return pd.DataFrame({'Hal_ids':docid_coll,'DOIs':dois_coll,'Titres':titres_coll, 'Types de dépôts':submit_types_coll})
 
 # Fonction pour fusionner les lignes en gardant les valeurs identiques et en concaténant les valeurs différentes
 def merge_rows_with_sources(group):
@@ -327,20 +434,10 @@ def main():
             combined_df = pd.concat([scopus_df, openalex_df, pubmed_df], ignore_index=True)
 
             # Récupérer les données HAL
-            dois_coll, titres_coll = get_hal_data(collection_a_chercher, start_year, end_year)
-            nti_coll = [normalise(x).strip() for x in titres_coll]
-
-            # Ajouter la colonne "Statut"
-            combined_df['Statut'] = ''
-
-            for i, r in combined_df.iterrows():
-                print(f"\rTraité : {i} sur {len(combined_df)}", end="\t\t")
-                ret_doi = statut_doi(r.doi, dois_coll)
-                if ret_doi in ["pas de DOI valide", "hors HAL"]:
-                    ret_ti = statut_titre(r.Title, nti_coll)
-                    combined_df.loc[i, 'Statut'] = ret_ti
-                else:
-                    combined_df.loc[i, 'Statut'] = ret_doi
+            coll=HalCollImporter(collection_a_chercher,start_year,end_year)
+            coll_df=coll.import_data()
+            coll_df['nti']=coll_df['Titres'].apply(lambda x : normalise(x).strip())
+            check_df(combined_df,coll_df)
 
         # Étape 5 : Fusion des lignes en double
         with st.spinner("Fusion"):

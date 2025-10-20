885319d1d94b99503b2c0859f584021d4f1f22ac
l-jonchere <laurent.jonchere@univ-rennes.fr>
2025-10-20 13:25:43 +0200

    Update streamlit_app_rennes.py

--- streamlit_app_rennes.py
+++ streamlit_app_rennes.py
@@ -150,6 +150,7 @@ def add_sidebar_menu():
     Cette version est préconfigurée pour les laboratoires de l'Université de Rennes.
     Sélectionnez un laboratoire dans la liste pour lancer la comparaison de ses publications
     (Scopus, OpenAlex, PubMed) avec sa collection HAL. c2LabHAL est une application créée par Guillaume Godet (Nantes Univ)
+    st.sidebar.markdown("Contact : Laurent Jonchère (Univ Rennes)")
     """
 )
     st.sidebar.markdown("---")
